7f255bd62dc5e86610ce98d73952b96d066401f6
Sean McDermott and Susan Southall <susan@>
2018-04-23 13:37:13 -0700

    add functionality for sleep and attention attributes and passing spec

--- spec/tamagotchi-spec.js
+++ spec/tamagotchi-spec.js
@@ -6,26 +6,42 @@ describe('Tamagotchi', function() {
   beforeEach(function() {
     jasmine.clock().install();
     pico.setHunger();
+    pico.setSleep();
+    pico.setAttention();
   });
 
   afterEach(function() {
     jasmine.clock().uninstall();
   });
 
-  it('should have a name and a food level of 10 when it is created', function() {
+  it('should have a name and a food/sleep/attention level of 10 when it is created', function() {
     expect(pico.name).toEqual("Pico De Gallo");
     expect(pico.foodLevel).toEqual(10);
+    expect(pico.sleepLevel).toEqual(10);
+    expect(pico.attentionLevel).toEqual(10);
   });
 
   it('should have a food level of 7 after 3001 milliseconds', function() {
     jasmine.clock().tick(3001);
     expect(pico.foodLevel).toEqual(7);
+    expect(pico.sleepLevel).toEqual(7);
+    expect(pico.attentionLevel).toEqual(7);
   });
 
   it('should get very hungry if the food level drops below zero', function() {
-    fuzzy.foodLevel = 0;
+    pico.foodLevel = 0;
     expect(pico.zombieGotchi()).toEqual(true);
   });
+
+  it('should get very tired if the sleep level drops below zero', function() {
+    pico.sleepLevel = 1;
+    expect(pico.zombieGotchiSleep()).toEqual(false);
+  });
+
+  it('should get very lonely if the attention level drops below zero', function() {
+    pico.attentionLevel = 0;
+    expect(pico.zombieGotchiAttention()).toEqual(true);
+  })
 
   it('should get very hungry if 10 seconds pass without feeding', function() {
     jasmine.clock().tick(10001);
--- src/tamagotchi.js
+++ src/tamagotchi.js
@@ -1,23 +1,55 @@
 class Tamagotchi {
-  // constructor(name) {
-  //   this.name = name;
-  //   this.foodLevel = 10;
-  // }
-  // setHunger() {
-  //   setInterval(() => {
-  //     this.foodLevel--;
-  //   }, 1000);
-  // }
-  // didYouGetEaten() {
-  //   if (this.foodLevel > 0) {
-  //     return false;
-  //   } else {
-  //     return true;
-  //   }
-  // }
-  //
-  // feed() {
-  //   this.foodLevel = 10;
-  // }
+  constructor(name) {
+    this.name = name;
+    this.foodLevel = 10;
+    this.sleepLevel = 10;
+    this.attentionLevel = 10;
+  }
+  setHunger() {
+    setInterval(() => {
+      this.foodLevel--;
+    }, 1000);
+  }
+  setSleep() {
+    setInterval(() => {
+      this.sleepLevel--;
+    }, 1000);
+  }
+  setAttention() {
+    setInterval(() => {
+      this.attentionLevel--;
+    }, 1000);
+  }
+  zombieGotchi() {
+    if (this.foodLevel > 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+  zombieGotchiSleep() {
+    if (this.sleepLevel > 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+  zombieGotchiAttention() {
+    if (this.attentionLevel > 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  feed() {
+    this.foodLevel = 10;
+  }
+  sleep() {
+    this.sleepLevel = 10;
+  }
+  attention() {
+    this.attentionLevel = 10;
+  }
 }
 export { Tamagotchi };

f2595071892218cd76eca00ddcacb74f548a68b4
Sean McDermott and Susan Southall <susan@>
2018-04-24 16:09:08 -0700

    got the API call to work

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import {Tamagotchi} from "./tamagotchi.js";
+import { Tamagotchi } from "./tamagotchi.js";
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -6,5 +6,21 @@ import './styles.css';
 
 
 $(document).ready(function() {
-  $("#form-goes-here")
+  $("#feed").click(function() {
+
+    $.ajax({
+      url: `https://api.giphy.com/v1/stickers/search?q=hamtaro&api_key=${process.env.API_KEY}`,
+      type: 'GET',
+      data: {
+        format: 'json'
+      },
+      success: function(response) {
+        console.log(response);
+        $('.hamtaroImage').append(`<img src="${response.data[7].images.downsized.url}" alt="giphy">`);
+      },
+      error: function() {
+        $('#errors').text("There was an error processing your request. Please try again.")
+      }
+    });
+  });
 });
